f30ee7eb59389e7ebfc1149bf8d886c116b3a42e
Renam Gustavo Bulhoes <renamgustavo@live.com>
2023-03-04 16:32:24 -0300

    Projeto criado

---
+++ src/main/java/com/github/renamrgb/flashcardsdio/FlashcardsDioApplication.java
@@ -0,0 +1,13 @@
+package com.github.renamrgb.flashcardsdio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlashcardsDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlashcardsDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/renamrgb/flashcardsdio/FlashcardsDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.renamrgb.flashcardsdio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardsDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
